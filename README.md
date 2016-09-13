544da18c0d8de8fcd6dea74c915d98d4560bd04d
Darlan Mendonça <darlanmendonca@gmail.com>
2016-09-13 10:32:49 -0300

    initial files and first draft

---
+++ README.md
@@ -0,0 +1,45 @@
+# Minimalist input checkbox
+
+Minimalist input radio component, agnostic to framworks.
+
+<!-- See the [demo](http://codepen.io/darlanmendonca/full/xEwkaz) -->
+
+<!-- [![preview demo](https://raw.githubusercontent.com/minimalist-components/mn-radio/master/sources/example/mn-radio.gif)](http://codepen.io/darlanmendonca/full/akgXQq)  -->
+
+### Install
+
+With bower
+
+```sh
+bower install --save mn-radio
+```
+
+Or just download the main files ```dist/mn-radio.css, dist/mn-radio.js``` in your project, and make a reference to their, like:
+
+```html
+<link rel="stylesheet" href="path/to/dist/mn-radio.css">
+<script src="path/to/dist/mn-radio.css"></script>
+```
+
+### Usage
+
+And then, in your html, you can use the tag ```mn-radio``` i.e.
+
+```html
+<mn-radio label="Female" name="gender"></mn-radio>
+<mn-radio label="Male" name="gender"></mn-radio>
+```
+
+Custom attributes
+
+- label - custom attribute, to define the text visible for radio, like example above
+
+The following attributes from input type radio are supported in this component
+
+
+- [autofocus](http://www.w3schools.com/tags/att_input_autofocus.asp)
+- [checked](http://www.w3schools.com/tags/att_input_checked.asp)
+- [disabled](http://www.w3schools.com/tags/att_input_disabled.asp)
+- [name](http://www.w3schools.com/tags/att_input_name.asp)
+- [required](http://www.w3schools.com/tags/att_input_required.asp)
+- [value](http://www.w3schools.com/tags/att_input_value.asp)
---
+++ sources/scripts/mn-radio.component.js
@@ -0,0 +1,70 @@
+'use strict';
+
+let prototype = Object.create(HTMLElement.prototype);
+prototype.createdCallback = mnRadio;
+document.registerElement('mn-radio', {prototype});
+
+function mnRadio() {
+  let element = this;
+
+  let inputAttributes = [
+    {
+      name: 'type',
+      default: 'radio',
+    },
+    {
+      name: 'name',
+    },
+    {
+      name: 'autofocus',
+    },
+    {
+      name: 'checked',
+    },
+    {
+      name: 'disabled',
+    },
+    {
+      name: 'value',
+    },
+  ];
+
+  // label element
+  let labelText = element.getAttribute('label');
+  let label = document.createElement('label');
+  let text = document.createElement('span');
+  if (labelText) {
+    text.textContent = element.getAttribute('disabled')
+      ? `${labelText} disabled`
+      : labelText;
+  }
+  label.appendChild(text);
+
+  // input element
+  let input = document.createElement('input');
+  inputAttributes.map(setInputAttribute);
+  label.appendChild(input);
+
+  let radio = document.createElement('div');
+  radio.className = 'radio';
+  label.appendChild(radio);
+  element.appendChild(label);
+
+  function setInputAttribute(attribute) {
+    let isDefaultAttribute = attribute.hasOwnProperty('default');
+    let attributeValue = element.getAttribute(attribute.name);
+
+    if (isDefaultAttribute) {
+      let isValidValue = attribute.hasOwnProperty('values')
+        && attribute.values.indexOf(attributeValue) >= 0;
+
+      let value = isValidValue
+        ? attributeValue
+        : attribute.default;
+
+      input.setAttribute(attribute.name, value);
+    } else if (attributeValue) {
+      input.setAttribute(attribute.name, attributeValue);
+    }
+  }
+}
