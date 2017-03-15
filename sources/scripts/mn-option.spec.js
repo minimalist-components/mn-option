88e78d159bb2db5c66576dfb6d441a172bde4d4f
Darlan Mendonça <darlanmendonca@gmail.com>
2017-03-15 16:36:33 -0300

    add spec "change value by click"

--- sources/scripts/mn-option.spec.js
+++ sources/scripts/mn-option.spec.js
@@ -5,24 +5,34 @@ import {Selector} from 'testcafe'
 
 chai.use(chaiAsPromised)
 
-fixture `undefined values`
+fixture `mn-option[type=radio]`
   .page('http://localhost:8080/docs/demo.html')
 
-test('radio value is undefined', async page => {
-  const list = await Selector('mn-option[name="list"]')
-  await expect(list.value).to.eventually.be.undefined
+test('value is undefined', async page => {
+  const radio = await Selector('mn-option[name="list"]')
+  await expect(radio.value).to.eventually.be.undefined
+  await expect(radio.hasClass('checked')).to.eventually.be.false
+})
+
+test('change value by click', async page => {
+  const stark = await Selector('mn-option[name="list"][value="stark"]')
+  const lannister = await Selector('mn-option[name="list"][value="lannister"]')
+
+  await page.click(stark.find('label'))
+  await expect(stark.value).to.eventually.be.equal('stark')
+  await expect(stark.hasClass('checked')).to.eventually.be.true
+  await expect(lannister.hasClass('checked')).to.eventually.be.false
+
+  await page.click(lannister.find('label'))
+  await expect(stark.value).to.eventually.be.equal('lannister')
+  await expect(stark.hasClass('checked')).to.eventually.be.false
+  await expect(lannister.hasClass('checked')).to.eventually.be.true
 })
 
 test('checkbox value is array', async () => {
   const list2 = await Selector('mn-option[name="list2"]')
   await expect(list2.value).to.eventually.be.an('array')
   await expect(list2.value).to.eventually.be.empty
-})
-
-test('change value in radio', async page => {
-  const list = await Selector('mn-option[name="list"]')
-  await page.click(list.find('label'))
-  await expect(list.value).to.eventually.be.equal('stark')
 })
 
 
