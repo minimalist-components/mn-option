fd8192a6e102912ce5a150361c3ad1160c3728ef
Darlan Mendonça <darlanmendonca@gmail.com>
2017-03-15 20:49:56 -0300

    better write with eventually

--- sources/scripts/mn-option.spec.js
+++ sources/scripts/mn-option.spec.js
@@ -12,22 +12,22 @@ fixture `type radio`
   test('value is a undefined by default', async page => {
     const {radioList} = demo.radio
 
-    await expect(radioList.value).to.eventually.be.undefined
-    await expect(radioList.hasClass('checked')).to.eventually.be.false
+    await expect(radioList.value).to.be.eventually.undefined
+    await expect(radioList.hasClass('checked')).to.be.eventually.false
   })
 
   test('change value by click', async page => {
     const {optionStark, optionLannister} = demo.radio
 
     await page.click(optionStark.find('label'))
-    await expect(optionStark.value).to.eventually.be.equal('stark')
-    await expect(optionStark.hasClass('checked')).to.eventually.be.true
-    await expect(optionLannister.hasClass('checked')).to.eventually.be.false
+    await expect(optionStark.value).to.be.eventually.equal('stark')
+    await expect(optionStark.hasClass('checked')).to.be.eventually.true
+    await expect(optionLannister.hasClass('checked')).to.be.eventually.false
 
     await page.click(optionLannister.find('label'))
-    await expect(optionStark.value).to.eventually.be.equal('lannister')
-    await expect(optionStark.hasClass('checked')).to.eventually.be.false
-    await expect(optionLannister.hasClass('checked')).to.eventually.be.true
+    await expect(optionStark.value).to.be.eventually.equal('lannister')
+    await expect(optionStark.hasClass('checked')).to.be.eventually.false
+    await expect(optionLannister.hasClass('checked')).to.be.eventually.true
   })
 
 fixture `type checkbox`
@@ -36,22 +36,22 @@ fixture `type checkbox`
   test('value is an array by default', async () => {
     const {checkboxList} = demo.checkbox
 
-    await expect(checkboxList.value).to.eventually.be.an('array')
-    await expect(checkboxList.value).to.eventually.be.empty
+    await expect(checkboxList.value).to.be.eventually.an('array')
+    await expect(checkboxList.value).to.be.eventually.empty
   })
 
   test('change value by click', async page => {
     const {optionNed, optionDaenerys} = demo.checkbox
 
     await page.click(optionNed.find('label'))
-    await expect(optionNed.value).to.eventually.deep.equal(['ned'])
-    await expect(optionNed.hasClass('checked')).to.eventually.be.true
-    await expect(optionDaenerys.hasClass('checked')).to.eventually.be.false
+    await expect(optionNed.value).to.deep.eventually.equal(['ned'])
+    await expect(optionNed.hasClass('checked')).to.be.eventually.true
+    await expect(optionDaenerys.hasClass('checked')).to.be.eventually.false
 
     await page.click(optionDaenerys.find('label'))
-    await expect(optionNed.value).to.eventually.deep.equal(['ned', 'daenerys'])
-    await expect(optionNed.hasClass('checked')).to.eventually.be.true
-    await expect(optionDaenerys.hasClass('checked')).to.eventually.be.true
+    await expect(optionNed.value).to.deep.eventually.equal(['ned', 'daenerys'])
+    await expect(optionNed.hasClass('checked')).to.be.eventually.true
+    await expect(optionDaenerys.hasClass('checked')).to.be.eventually.true
   })
 
 
