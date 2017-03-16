446a8af93da41146a7a95a13fb6713e6ed76322b
Darlan Mendonça <darlanmendonca@gmail.com>
2017-03-16 17:14:59 -0300

    add browserstack provider to dependencies

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,7 @@ set -e
 
 : ${browser:=nightmare}
 
-./node_modules/.bin/testcafe $browser ./sources/scripts/*.spec.js --app './node_modules/.bin/http-server . -s'
+./node_modules/.bin/testcafe $browser \
+./sources/scripts/*.spec.js \
+--app './node_modules/.bin/http-server . -s'
+
