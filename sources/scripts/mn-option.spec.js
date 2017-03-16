719eb7f21af96ba08f85cb5e0d9b7b1bafe795a9
Darlan Mendonça <darlanmendonca@gmail.com>
2017-03-16 11:44:31 -0300

    refactor and keep a single page object

--- sources/scripts/mn-option.spec.js
+++ sources/scripts/mn-option.spec.js
@@ -3,9 +3,7 @@ import {expect} from 'chai'
 import chaiAsPromised from 'chai-as-promised'
 import chaiDom from 'chai-dom'
 import {Selector} from 'testcafe'
-import demo from '../../test/e2e/demo/demo.po.js'
-import RadioPageObject from '../../test/e2e/demo/radio.po.js'
-import CheckboxPageObject from '../../test/e2e/demo/checkbox.po.js'
+import MnOptionPageObject from './mn-option.po.js'
 
 chai
   .use(chaiDom)
@@ -15,58 +13,50 @@ let radio
 let checkbox
 
 fixture `type radio`
-  .page(demo.url)
+  .page('http://localhost:8080/docs/demo.html')
   .beforeEach(page => {
-    radio = new RadioPageObject(page)
+    radio = new MnOptionPageObject('list', page)
   })
 
-  test.only('value is a undefined by default', async page => {
-    const {radioList} = radio
-
-    await expect(radioList.value).to.be.eventually.undefined
-    await expect(radioList.hasClass('checked')).to.be.eventually.false
+  test('value is a undefined by default', async page => {
+    await expect(radio.options.value).to.be.eventually.undefined
+    await expect(radio.options.hasClass('checked')).to.be.eventually.false
   })
 
-  test.only('change value by click', async page => {
-    const {radioList} = radio
-
+  test('change value by click', async page => {
     await radio.clickOnOptionWithValue('stark')
-    await expect(radioList.value).to.be.eventually.equal('stark')
+    await expect(radio.options.value).to.be.eventually.equal('stark')
     await expect(radio.option('stark').hasClass('checked')).to.be.eventually.true
     await expect(radio.option('lannister').hasClass('checked')).to.be.eventually.false
 
-    // await page.click(optionLannister.find('label'))
-    // await expect(optionStark.value).to.be.eventually.equal('lannister')
-    // await expect(optionStark.hasClass('checked')).to.be.eventually.false
-    // await expect(optionLannister.hasClass('checked')).to.be.eventually.true
+    await radio.clickOnOptionWithValue('lannister')
+    await expect(radio.options.value).to.be.eventually.equal('lannister')
+    await expect(radio.option('stark').hasClass('checked')).to.be.eventually.false
+    await expect(radio.option('lannister').hasClass('checked')).to.be.eventually.true
   })
 
 fixture `type checkbox`
-  .page(demo.url)
-  .before(page => {
-    radio = new RadioPageObject(page)
+  .page('http://localhost:8080/docs/demo.html')
+  .beforeEach(page => {
+    checkbox = new MnOptionPageObject('list2', page)
   })
 
   test('value is an array by default', async () => {
-    const {checkboxList} = demo.checkbox
-
-    await expect(checkboxList.value).to.be.eventually.an('array')
-    await expect(checkboxList.value).to.have.eventually.lengthOf(0)
+    await expect(checkbox.options.value).to.be.eventually.an('array')
+    await expect(checkbox.options.value).to.have.eventually.lengthOf(0)
     // await expect(checkboxList.value).to.be.eventually.empty
   })
 
   test('change value by click', async page => {
-    const {optionNed, optionDaenerys} = demo.checkbox
+    await checkbox.clickOnOptionWithValue('ned')
+    await expect(checkbox.option('ned').value).to.deep.eventually.equal(['ned'])
+    await expect(checkbox.option('ned').hasClass('checked')).to.be.eventually.true
+    await expect(checkbox.option('daenerys').hasClass('checked')).to.be.eventually.false
 
-    await page.click(optionNed.find('label'))
-    await expect(optionNed.value).to.deep.eventually.equal(['ned'])
-    await expect(optionNed.hasClass('checked')).to.be.eventually.true
-    await expect(optionDaenerys.hasClass('checked')).to.be.eventually.false
-
-    await page.click(optionDaenerys.find('label'))
-    await expect(optionNed.value).to.deep.eventually.equal(['ned', 'daenerys'])
-    await expect(optionNed.hasClass('checked')).to.be.eventually.true
-    await expect(optionDaenerys.hasClass('checked')).to.be.eventually.true
+    await checkbox.clickOnOptionWithValue('daenerys')
+    await expect(checkbox.option('ned').value).to.deep.eventually.equal(['ned', 'daenerys'])
+    await expect(checkbox.option('ned').hasClass('checked')).to.be.eventually.true
+    await expect(checkbox.option('daenerys').hasClass('checked')).to.be.eventually.true
   })
 
 
--- test/e2e/demo/demo.po.js
+++
@@ -1,9 +0,0 @@
-import {Selector} from 'testcafe'
-
-module.exports = new PageObject()
-
-function PageObject() {
-  const url = 'http://localhost:8080/docs/demo.html'
-
-  return {url}
-}
