fa2273484ef2fb15768f73e055f298e98519a80c
Darlan Mendonça <darlanmendonca@gmail.com>
2017-03-17 11:50:41 -0300

    add simple test in chrome in travis

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,9 @@ set -e
 
 : ${browser:=nightmare}
 
-./node_modules/.bin/testcafe \"$browser\" \
+echo $browser
+
+./node_modules/.bin/testcafe $browser \
 ./sources/scripts/*.spec.js \
 --app './node_modules/.bin/http-server . -s'
 
