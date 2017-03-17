cdc2c8b0827ebaf45ae5352853b5dff9800f4be3
Darlan Mendonça <darlanmendonca@gmail.com>
2017-03-17 11:10:19 -0300

    scape $browser in test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,7 @@ set -e
 
 : ${browser:=nightmare}
 
-./node_modules/.bin/testcafe $browser \
+./node_modules/.bin/testcafe \"$browser\" \
 ./sources/scripts/*.spec.js \
 --app './node_modules/.bin/http-server . -s'
 
